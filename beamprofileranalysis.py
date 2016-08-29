efb734e970782eb3d2a80fd9c4752fa50c707ed8
Kyle Air <cpkmanchee@gmail.com>
2016-08-29 17:25:58 -0400

    rough plotting in comments

--- beamprofileranalysis.py
+++ beamprofileranalysis.py
@@ -165,4 +165,23 @@ for f in files:
     '''
 
 plt.show()
+
+
+'''
+# plot twoD_Gaussian data generated above
+plt.figure()
+plt.imshow(data.reshape(201, 201))
+plt.colorbar()
+
+#plot result
+data_fitted = twoD_Gaussian((x, y), *popt)
+
+fig, ax = plt.subplots(1, 1)
+ax.hold(True)
+ax.imshow(data_noisy.reshape(201, 201), cmap=plt.cm.jet, origin='bottom',
+    extent=(x.min(), x.max(), y.min(), y.max()))
+ax.contour(x, y, data_fitted.reshape(201, 201), 8, colors='w')
+plt.show()
+'''
+
     
