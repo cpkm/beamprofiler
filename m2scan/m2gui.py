13f121c8fcd097e1b820bb8629a0cd3ecb13dbcf
Kyle Air <cpkmanchee@gmail.com>
2016-09-21 22:41:16 -0400

    i dunno lots of m2 camera shit

--- m2scan/m2gui.py
+++ m2scan/m2gui.py
@@ -7,36 +7,46 @@ Created on Fri Jun  6 16:50:02 2014
 
 import os
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import threading
+import time
 
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename
-from PIL import Image
+from PIL import Image, ImageTk
 
 
-class Application(Frame):
+class Application(Tk):
     """ GUI app to aid in the collection of beam profile data"""
     
-    def __init__(self, master):
+    def __init__(self, master=None):
         """Initialize frame"""
-        Frame.__init__(self,master)
-        self.grid()
-        self.create_widgets()
+        root = self.root = Tk.__init__(self,master)
         
-    def create_widgets(self):
+        self.geometry('425x350')
+        self.protocol("WM_DELETE_WINDOW", self.onClose)
+        
+        self.grid()
+        self.createWidgets()
+        
+    def createWidgets(self):
         """Create button, text, entry widgets"""
         
+        self.thread = None
+        self.stopEvent = None  
+
         self.camSelectLabel = Label(self, text = 'Select camera:')
         self.camSelectLabel.grid(row = 0, column = 0, columnspan = 1, sticky = W)
         
-        camera = StringVar()
-        camera.set('--Select--')
-        
+        self.camera_name = StringVar()
+        self.camera_name.set('-Select-')
         camera_list = self.getCameras()
         
-        self.camSelect = OptionMenu(self, camera, *camera_list)
+        self.camSelect = OptionMenu(self, self.camera_name, *camera_list, command = self.startPreview)
         self.camSelect.grid(row = 0, column = 1, columnspan = 1, sticky = W)        
         
-        self.cameraLabel = Label(self, textvariable = camera)
+        self.cameraLabel = Label(self, textvariable = self.camera_name)
         self.cameraLabel.grid(row=0, column=2, columnspan = 1, sticky = W)
         
         self.saveLabel = Label(self, text = 'Save location:')
@@ -54,12 +64,31 @@ class Application(Frame):
         self.capture = Button(self, text='Capture', command = self.onCapture)
         self.capture.grid(row=2,column=0, sticky = W)
         
-        self.text = Text(self, width = 35, height = 5, wrap = WORD)
-        self.text.grid(row=3, column=0, columnspan = 2, sticky = W)
+        img = None
+        self.previewPanel = Label(self, image = img)
+        self.previewPanel.image = img
+        self.previewPanel.grid(row=3, column=0, columnspan=4, sticky = W)
+        
+         
+         
+    def onClose(self):
+        '''what to do when window is closed'''
+        self.stopPreview()
+        self.destroy()
         
     def onCapture(self):
         """Display message based on password input"""
         
+        self.stopPreview()
+        print('Stopped Preview')
+        
+        print(self.stopEvent.is_set())
+        
+        #time.sleep(10)
+        self.startPreview(self.camera_name.get())
+        #print('Yup started preview')
+        
+        '''
         #im = captureImage()
         img = np.array([[[0,0,0]]]) #1-pixel image
         img = Image.fromarray(img, 'RGB')
@@ -68,10 +97,84 @@ class Application(Frame):
             i += 1
         filename = self.saveDir.get() + '%03d.jpeg' % i
         img.save(filename) #this depends on what I use to get the image
+        '''
+        
+    def startPreview(self, camera_number):
+        '''start camera preview'''
+        #print(self.stopEvent.is_set())
+        self.stopEvent = None
+        self.thread = None
+        self.thread = threading.Thread(target=self.videoLoop, args=())
+        self.stopEvent = threading.Event()
+        
+        self.cam = cv2.VideoCapture(camera_number)
+        
+        self.thread.start()
+        print('Preview started')
+
+    def stopPreview(self):
+        
+        try:
+            self.stopEvent.set()
+            print('Set thread stop signal')
+            self.previewPanel.configure(image = None)
+            print('Removed preview image')
+            self.cam.release()
+            print('Released camera')
+        except AttributeError:
+            print('Camera not connected')
+       
+    def videoLoop(self):
+        
+        try:
+            if not self.stopEvent.is_set():
+                # Capture frame-by-frame
+                r,frame = self.cam.read()
+                
+                if r:
+                    width = 400
+                    frame_sized = cv2.resize(frame, (width, np.int(9*width/16)))  
+                
+                    # Display the resulting frame
+                    #cv2.imshow(WINDOW_NAME,frame_sized)
+                
+                    frame_sized = cv2.cvtColor(frame_sized,cv2.COLOR_BGR2RGBA)
+                    image = Image.fromarray(frame_sized)
+                    img = ImageTk.PhotoImage(image)
+                    
+                    self.previewPanel.configure(image = img)
+                    self.previewPanel.image = img 
+                
+                if not self.stopEvent.is_set():
+                    self.previewPanel.after(10, self.videoLoop())
+                    
+        except AssertionError:
+            print('Some dumb AssertionError')
+            
+        except RuntimeError:
+            print('WTF is a runtime error?')
+            raise
+        
+        except:
+            print('Probably some stupid cv2 error... threading... yada, yada, yada')
+            raise
+#        cv2.waitKey(1)
+
         
     def getCameras(self):
         '''get list of available cameras'''
-        camera_list = ['cam 0', 'cam 1']
+
+        camNum = 0
+        camera_list =[]
+        while camNum < 3:
+            cam = cv2.VideoCapture(camNum)
+            open_bool = cam.isOpened()
+        
+            if open_bool:
+                camera_list.append(camNum)
+        
+            camNum += 1   
+            
         return camera_list
         
     def browseDir(self):
@@ -80,10 +183,16 @@ class Application(Frame):
         self.saveDir.set(directory)
         
         
+'''
 root = Tk()
 root.title("Beam Profile Collection")
-root.geometry('400x300')
+root.geometry('425x350')
 
 app = Application(root)
 
 root.mainloop()
+'''
+
+app = Application()
+app.title("Beam Profile Collection")
+app.mainloop()
