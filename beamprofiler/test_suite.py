d88aeb2063c63dbd58621f4189fed4cc4d7f1b5e
Kyle Air <cpkmanchee@gmail.com>
2017-12-03 21:28:03 -0500

    mc for fitting is complete.

--- beamprofiler/test_suite.py
+++ beamprofiler/test_suite.py
@@ -31,11 +31,6 @@ def test_fit(z,val_in,na,wl_in):
 
     return val,err,d_in
 
-## Test M2 fitting
-f1,(ax1,ax2) = plt.subplots(2,1)
-rows = ['z0', 'd0', 'M2', 'theta', 'zR']
-cols = ['Actual', 'Fit', 'Fit Sigma']
-
 #Define input test points
 z0_in = 0
 d0_in = 50.0E-6
@@ -45,36 +40,47 @@ zR_in = np.pi*(d0_in/2)**2/(wl_in*M2_in)
 theta_in = 2*M2_in*wl_in/(np.pi*d0_in/2)
 val_in = [z0_in, d0_in, M2_in, theta_in, zR_in]
 
-# Noise amplitude
-num_pts=20
-na = 0.02
+N = 1000
+
+num_pts=10
+na = 0.05
 nd = np.floor(num_pts/4)
 
 #create symmetric spacial array within ISO spec.
 z = np.concatenate((np.linspace(-4*zR_in,-2*zR_in,nd), np.linspace(-zR_in,zR_in,2*nd), np.linspace(2*zR_in,4*zR_in,nd)))
 
+print(np.size(val_in))
 #Perform test
-v_avg=0
-v_std=0
-s_avg=0
-s_std=0
+v_sum=np.zeros(np.size(val_in))
+v2_sum=np.zeros(np.size(val_in))
+s_sum=np.zeros(np.size(val_in))
+s2_sum=np.zeros(np.size(val_in))
 
 for i in range(N):
     v,s,_ = test_fit(z,val_in,na,wl_in)
 
-    v_avg = v_avg + v/(i+1)
-    v_std = v_avg + v/(i+1)
-    s_avg = s_avg + s/(i+1)
-    s_std = v_avg + s/(i+1)
+    v_sum = v_sum + np.array(v)
+    v2_sum = v2_sum + np.array(v)**2
+    s_sum = s_sum + np.array(s)
+    s2_sum = s2_sum + np.array(s)**2
+
+val_avg = v_sum/N
+val_std = ((1/(N-1))*(v2_sum - v_sum**2/N))**(1/2)
+
+std_avg = s_sum/N
+std_std = ((1/(N-1))*(s2_sum - s_sum**2/N))**(1/2)
 
 #Plot results
-ax1.plot(z,d_in,'bx')
-ax1.plot(z,2*gaussianbeamwaist(z,*val[:3], wl_in))
+f1,ax2 = plt.subplots(1,1)
+rows = ['z0', 'd0', 'M2', 'theta', 'zR']
+cols = ['Actual', 'Fit', 'MCsig', 'Fit Sigma','MCsig']
+
 ax2.axis('tight')
 ax2.axis('off')
-ax2.table(cellText=np.transpose([val_in,val,std]), rowLabels = rows, colLabels = cols, loc='center')
+ax2.table(cellText=np.transpose([val_in,val_avg,val_std,std_avg,std_std]), rowLabels = rows, colLabels = cols, loc='center')
 plt.show()
 #Create table to compare values
 
-
+#ax1.plot(z,d_in,'bx')
+#ax1.plot(z,2*gaussianbeamwaist(z,*val[:3], wl_in))
 ## Test image processing
